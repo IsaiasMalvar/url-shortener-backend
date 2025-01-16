cca68b7d59bb90ea4ac88202caed2e6ed5a14d90
Isaias Malvar <isaiasmg@93hotmail.com>
2025-01-16 13:55:52 +0000

    Create UrlMapping model

--- src/main/java/com/urlized/shortener/models/UrlMapping.java
+++ src/main/java/com/urlized/shortener/models/UrlMapping.java
@@ -1,4 +1,24 @@
 package com.urlized.shortener.models;
 
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class UrlMapping {
+
+    private Long id;
+    private String originalUrl;
+    private String shortUrl;
+    private int clickCount = 0;
+    private LocalDateTime createdDate;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "urlMapping")
+    private List<ClickEvent> clickEvents;
 }
