6ca40b3f52b3aabaf5bb18dc751e16ef9f2b79d7
Isaias Malvar <isaiasmg@93hotmail.com>
2025-01-18 00:02:47 +0000

    Create login endpoint and its functionality

--- src/main/java/com/urlized/shortener/controller/AuthController.java
+++ src/main/java/com/urlized/shortener/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.urlized.shortener.controller;
 
+import com.urlized.shortener.dtos.LoginRequest;
 import com.urlized.shortener.dtos.RegisterRequest;
 import com.urlized.shortener.models.User;
 import com.urlized.shortener.service.UserService;
@@ -16,6 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private UserService userService;
+
+    @PostMapping("/public/login")
+    public ResponseEntity<?> loginUser(@RequestBody LoginRequest loginRequest){
+        return ResponseEntity.ok(userService.authenticateUser(loginRequest));
+    }
 
     @PostMapping("/public/register")
     public ResponseEntity<?> registerUser(@RequestBody RegisterRequest registerRequest){
