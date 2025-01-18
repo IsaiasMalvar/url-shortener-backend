647dbcdca86e4f8aa3890835c6c2728e36d4eef8
Isaias Malvar <isaiasmg@93hotmail.com>
2025-01-18 16:03:27 +0000

    Create endpoint and service functionality to fetch all URL mappings

--- src/main/java/com/urlized/shortener/service/UrlMappingService.java
+++ src/main/java/com/urlized/shortener/service/UrlMappingService.java
@@ -8,6 +8,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Random;
 
 @AllArgsConstructor
@@ -47,5 +48,9 @@ public class UrlMappingService {
             shortUrl.append(validCharacters.charAt(random.nextInt(validCharacters.length())));
         }
         return shortUrl.toString();
+    }
+
+    public List<UrlMappingDTO> getUrlsByUser(User user) {
+        return urlMappingRepository.findByUser(user).stream().map(this::convertToDTO).toList();
     }
 }
